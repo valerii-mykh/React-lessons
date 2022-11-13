7ae5483275bb4b11cd74a869e246d964c7cabc7a
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-13 18:22:53 +0200

    add

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const painting = {
+  <div>
+  <img src="" alt="" width="480" />
+  <h2></h2>
+  <p>Автор: <a href=""></a></p>
+  <p>Цена: кредитов</p>
+  <p>Доступность: заканчиваєтся или есть в наличии</p>
+  <button type="button">Добавить в корзину</button>
+</div>
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+
+
+
+
+{/* <div>
+  <img src="" alt="" width="480" />
+  <h2></h2>
+  <p>Автор: <a href=""></a></p>
+  <p>Цена: кредитов</p>
+  <p>Доступность: заканчиваєтся или есть в наличии</p>
+  <button type="button">Добавить в корзину</button>
+</div> */}
+
+
+
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
